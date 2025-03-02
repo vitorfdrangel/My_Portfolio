68b84f494727274342ef6cacd1df5430b52bc0f4
Vitor Rangel <vitorfdrangel@gmail.com>
2025-03-01 21:46:39 -0300

    Inciando portifólio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center flex-col">
+        <h1 className="text-center -mt-20">Olá! Eu sou o Vitor 😉</h1>
+        <h2>Meu portifólio estará aqui em breve...</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
