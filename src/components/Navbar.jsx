9240d76ac1629154ddd09b525d3c94b387dec1f2
Vitor Rangel <vitorfdrangel@gmail.com>
2025-03-03 01:00:26 -0300

    Navbar finalizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
 
   return (
     <>
-      {!openBar ? (
-        <div className="bg-neutral-800 w-full h-auto flex items-center justify-between px-5 rounded-xl">
+      <div className="bg-neutral-800 w-full max-w-7xl mx-auto h-auto px-5 sm:px-10 rounded-xl">
+        <div className="w-full flex items-center justify-between">
           <div>
             <img
               src="src/assets/logo.png"
@@ -19,54 +19,84 @@ const Navbar = () => {
           </div>
 
           <div>
-            <div className="hidden md:flex gap-8 font-semibold">
-              <a href="#">Home</a>
-              <a href="#">Sobre</a>
-              <a href="#">Skills</a>
-              <a href="#">Contato</a>
+            <div className="hidden sm:flex gap-8 text-lg font-semibold">
+              <a
+                href="#"
+                className="text-slate-100/80 hover:text-slate-100 duration-300"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-slate-100/80 hover:text-slate-100 duration-300"
+              >
+                Sobre
+              </a>
+              <a
+                href="#"
+                className="text-slate-100/80 hover:text-slate-100 duration-300"
+              >
+                Skills
+              </a>
+              <a
+                href="#"
+                className="text-slate-100/80 hover:text-slate-100 duration-300"
+              >
+                Contato
+              </a>
             </div>
 
-            <div>
-              <button
-                className="text-3xl cursor-pointer md:hidden"
-                onClick={() => setOpenBar(true)}
-              >
-                <FaBars />
-              </button>
-            </div>
+            {!openBar ? (
+              <div>
+                <button
+                  className="text-3xl cursor-pointer sm:hidden"
+                  onClick={() => setOpenBar(true)}
+                >
+                  <FaBars />
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button
+                  className="text-4xl cursor-pointer sm:hidden"
+                  onClick={() => setOpenBar(false)}
+                >
+                  <MdOutlineClose />
+                </button>
+              </div>
+            )}
           </div>
         </div>
-      ) : (
-        <div className="md:hidden bg-neutral-800 w-full h-auto px-3 rounded-xl flex flex-col">
-          <div className="flex items-center justify-between px-2">
-            <div>
-              <img
-                src="src/assets/logo.png"
-                alt="Logo"
-                className="w-20 h-auto -ml-3"
-              />
-            </div>
 
-            <div>
-              <button
-                className="text-4xl cursor-pointer md:hidden"
-                onClick={() => setOpenBar(false)}
-              >
-                <MdOutlineClose />
-              </button>
-            </div>
-          </div>
-
-          <div className="w-full border-t-1 border-neutral-600 flex flex-col gap-6 pt-6">
-            <a href="#" className=" w-full hover:bg-neutral-600">
+        {openBar && (
+          <div className="sm:hidden w-full border-t-1 font-semibold border-neutral-600 mt-2 py-6 flex flex-col gap-2">
+            <a
+              href="#"
+              className="w-full pl-8 py-3 hover:bg-neutral-700/20 duration-300 rounded-lg"
+            >
               Home
             </a>
-            <a href="#">Sobre</a>
-            <a href="#">Skills</a>
-            <a href="#">Contato</a>
+            <a
+              href="#"
+              className="w-full pl-8 py-3 hover:bg-neutral-700/20 duration-300 rounded-lg"
+            >
+              Sobre
+            </a>
+            <a
+              href="#"
+              className="w-full pl-8 py-3 hover:bg-neutral-700/20 duration-300 rounded-lg"
+            >
+              Skills
+            </a>
+            <a
+              href="#"
+              className="w-full pl-8 py-3 hover:bg-neutral-700/20 duration-300 rounded-lg"
+            >
+              Contato
+            </a>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
