ad704ba669298b0863fb9777af00754732b8831f
Vitor Rangel <vitorfdrangel@gmail.com>
2025-03-08 11:52:05 -0300

    Ajustes animate sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import Skills from "./components/Skills.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
 import UpBtn from "./components/UpBtn.jsx";
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function App() {
-  const [showSk, setShowSk] = useState(false);
-  const [showProj, setShowProj] = useState(false);
-  const [showCont, setShowCont] = useState(false);
-
   const abtRef = useRef(null);
   const skRef = useRef(null);
   const projRef = useRef(null);
@@ -42,21 +38,29 @@ function App() {
     }
   }, []);
 
+  const observer = new IntersectionObserver((entries) => {
+    let id = entries[0].target.id;
+    let classlist = entries[0].target.classList;
+
+    if (id == "showR") {
+      classlist.add("animate-showSectionRight");
+    }
+
+    if (id == "showL") {
+      classlist.add("animate-showSectionLeft");
+    }
+
+    if (id == "showB") {
+      classlist.add("animate-showSectionBottom");
+    }
+  });
+
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 470) {
-        setShowSk(true);
-      }
-
-      if (window.scrollY > 970) {
-        setShowProj(true);
-      }
-
-      if (window.scrollY > 2580) {
-        setShowCont(true);
-      }
+    Array.from(document.querySelectorAll(".reveal")).map((e) => {
+      observer.observe(e);
     });
   }, []);
+
   return (
     <div className="p-4 pb-14 flex flex-col gap-26 relative overflow-hidden">
       <header>
@@ -68,12 +72,18 @@ function App() {
           <About abtRef={abtRef} />
         </section>
 
-        <section>{showSk && <Skills skRef={skRef} />}</section>
+        <section id="showR" className="reveal">
+          <Skills skRef={skRef} />
+        </section>
 
-        <section>{showProj && <Projects projRef={projRef} />}</section>
+        <section id="showL" className="reveal">
+          <Projects projRef={projRef} />
+        </section>
       </main>
 
-      <footer>{showCont && <Contact cttRef={cttRef} />}</footer>
+      <footer id="showB" className="reveal">
+        <Contact cttRef={cttRef} />
+      </footer>
 
       <UpBtn />
     </div>
