e903f1fec6c46fd5b33ec6bf1447a52a7ffab00c
Vitor Rangel <vitorfdrangel@gmail.com>
2025-03-14 19:23:22 -0300

    Ajuste img

--- src/data/imgProjects.js
+++ src/data/imgProjects.js
@@ -1,13 +1,13 @@
-import img1 from "../assets/projectsImg/pass1.png";
-import img2 from "../assets/projectsImg/pass2.png";
-import img3 from "../assets/projectsImg/pass3.png";
-import img4 from "../assets/projectsImg/pass4.png";
-import img5 from "../assets/projectsImg/pass5.png";
-import img6 from "../assets/projectsImg/pass6.png";
-import img7 from "../assets/projectsImg/pass7.png";
-import img8 from "../assets/projectsImg/pass8.png";
-import img9 from "../assets/projectsImg/pass9.png";
-import img10 from "../assets/projectsImg/pass10.png";
+import img1 from "../img/foodImg/pass1.png";
+import img2 from "../img/foodImg/pass2.png";
+import img3 from "../img/foodImg/pass3.png";
+import img4 from "../img/foodImg/pass4.png";
+import img5 from "../img/foodImg/pass5.png";
+import img6 from "../img/foodImg/pass6.png";
+import img7 from "../img/foodImg/pass7.png";
+import img8 from "../img/foodImg/pass8.png";
+import img9 from "../img/foodImg/pass9.png";
+import img10 from "../img/foodImg/pass10.png";
 
 const projectFood = [
   { img: img1, id: "1" },
